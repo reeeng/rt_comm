c66199cea6e2e7c00ea0f49753bf37d76ebe7e30
Adipa Wijayathilaka <adipamihi@gmail.com>
2020-01-08 18:43:49 -0500

    Added docker config

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1.100-alpine AS build-env
+WORKDIR /app
+
+# Copy csproj and restore as distinct layers
+COPY RTComm/*.csproj ./
+RUN dotnet restore
+
+# Copy everything else and build
+COPY RTComm ./
+RUN dotnet publish -c Release -o out
+
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1.0-alpine
+WORKDIR /app
+
+ARG ASPNETCORE_ENVIRONMENT=Staging
+ENV ASPNETCORE_ENVIRONMENT ${ASPNETCORE_ENVIRONMENT}
+
+COPY --from=build-env /app/out .
+
+EXPOSE 5000
+
+ENTRYPOINT ["dotnet", "RTComm.dll"]
--- RTComm/Program.cs
+++ RTComm/Program.cs
@@ -18,11 +18,14 @@ namespace RTComm
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseUrls($"{(env.Equals("Development") ? "http://localhost:5000" : "https://0.0.0.0:5000")}");
+            });
+        }
     }
 }
