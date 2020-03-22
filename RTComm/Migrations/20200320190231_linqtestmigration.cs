9d85737b211637886f0115aba12dfab1a2826fb5
DonOsman <37080469+DonOsman@users.noreply.github.com>
2020-03-21 22:07:35 -0400

    Fixed Edit Functionality
    - Fixed editing functionality by having Comment and Event pages pass thru to a new page (commentedit and eventEdit) instead of eventadd and commentadd.razor

---
+++ RTComm/Migrations/20200320190231_linqtestmigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RTComm.Migrations
+{
+    public partial class linqtestmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
